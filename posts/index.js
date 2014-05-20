608be3f1bb74c67bc5acc5e2da592de2317bf928
Ionică Bizău <bizauionica@yahoo.com>
2014-05-20 09:39:22 +0300

    Reordered posts

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        "title": "Hello World"
-      , "slug": "hello-world"
-      , "publishedAt": "15-05-2014"
-      , "content": "hello-world.md"
-    }
-  , {
         "title": "Testing Code Highlight"
       , "slug": "testing-code-highlight"
       , "publishedAt": "18-05-2014"
       , "content": "testing-code-highlight.md"
+    }
+  , {
+        "title": "Hello World"
+      , "slug": "hello-world"
+      , "publishedAt": "15-05-2014"
+      , "content": "hello-world.md"
     }
 ];
