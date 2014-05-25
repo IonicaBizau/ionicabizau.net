3d35f5afb806bd716c2c7eb6847d6402ba158b0f
Ionică Bizău <bizauionica@yahoo.com>
2014-05-25 21:36:38 +0300

    Added BibleJS post

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,11 @@
 module.exports = [
     {
+        "title": "BibleJS NPM Module"
+      , "slug": "bible-js"
+      , "publishedAt": "25-05-2014"
+      , "content": "bible-js.md"
+    }
+  , {
         "title": "Testing Code Highlight"
       , "slug": "testing-code-highlight"
       , "publishedAt": "18-05-2014"
