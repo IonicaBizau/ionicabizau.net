039bda9c3ffb82749732ef1109c869c3a733d003
Ionică Bizău <bizauionica@yahoo.com>
2014-06-08 18:29:42 +0300

    Save name of md file in path field

--- posts/index.js
+++ posts/index.js
@@ -3,18 +3,18 @@ module.exports = [
         "title": "BibleJS NPM Module"
       , "slug": "bible-js"
       , "publishedAt": "25-05-2014"
-      , "content": "bible-js.md"
+      , "path": "bible-js.md"
     }
   , {
         "title": "Testing Code Highlight"
       , "slug": "testing-code-highlight"
       , "publishedAt": "18-05-2014"
-      , "content": "testing-code-highlight.md"
+      , "path": "testing-code-highlight.md"
     }
   , {
         "title": "Hello World"
       , "slug": "hello-world"
       , "publishedAt": "15-05-2014"
-      , "content": "hello-world.md"
+      , "path": "hello-world.md"
     }
 ];
