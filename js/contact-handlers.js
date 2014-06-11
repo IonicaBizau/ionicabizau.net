ff4335744f8115ae45afed5523043502ad06da13
Ionică Bizău <bizauionica@yahoo.com>
2014-06-11 16:03:56 +0300

    Added disableOnSubmit option

--- js/contact-handlers.js
+++ js/contact-handlers.js
@@ -2,13 +2,17 @@
     var formEl = document.getElementById("contact-form")
       , mbErrorEl = document.getElementById("mb-error")
       , mbSuccessEl = document.getElementById("mb-success")
-      , formToSubmit = new XHRFormSubmitter({form: formEl})
+      , formToSubmit = new XHRFormSubmitter({
+            form: formEl
+          , disableOnSubmit: true
+        })
       ;
 
     formEl.addEventListener("submit", function (e) {
         try {
             mbErrorEl.style.display = "none";
             mbSuccessEl.style.display = "none";
+
             formToSubmit.submit(function (err, data) {
 
                 var mb = null
