3dc22b8d002830961a058c75d7ac525eb39e20c1
Ionică Bizău <bizauionica@yahoo.com>
2014-06-13 18:08:56 +0300

    Moved the hardcoded HTML in template

--- template/index.js
+++ template/index.js
@@ -2,16 +2,21 @@
 var fs = require("fs");
 
 module.exports = {
-    page: fs.readFileSync(__dirname + "/pages.html")
+    pages: fs.readFileSync(__dirname + "/pages.html")
         .toString()
         .replace(
             "{{CSS_STYLE_URL}}"
           , SITE_CONFIG.paths.template + "/style.css"
         )
-  , post: fs.readFileSync(__dirname + "/posts.html")
+  , posts: fs.readFileSync(__dirname + "/posts.html")
         .toString()
         .replace(
             "{{CSS_STYLE_URL}}"
           , SITE_CONFIG.paths.template + "/style.css"
         )
+  , blocks: {
+        post: fs.readFileSync(__dirname + "/blocks/post.html").toString()
+      , page: fs.readFileSync(__dirname + "/blocks/page.html").toString()
+      , postContentEnd: fs.readFileSync(__dirname + "/blocks/post-content-end.html").toString()
+    }
 };
