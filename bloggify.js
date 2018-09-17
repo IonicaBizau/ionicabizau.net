4073dffa4f279fe286381c038ce893116ec9ba60
Ionică Bizău <bizauionica@gmail.com>
2018-09-17 15:30:50 +0300

    New configuration

--- bloggify.js
+++ bloggify.js
@@ -1,58 +1,49 @@
 "use strict";
 
-const conf = require("bloggify-config");
-
-module.exports = conf({
-    title: "Ionică Bizău"
-  , description: "Web Developer, <i class=\"fa fa-linux\"></i> *nix geek and <i class=\"fa fa-music\"></i> Musician"
-  , domain: "https://ionicabizau.net"
-  , theme: "bloggify-theme-light"
-  , plugins: [
-      "bloggify-emoji"
-    , "bloggify-lightbox"
-    , "bloggify-social"
-    , "bloggify-rss"
-    , "bloggify-redirect"
-    , "bloggify-custom-assets"
+module.exports = {
+    "title": "Ionică Bizău"
+  , "description": "Web Developer, <i class=\"fa fa-linux\"></i> *nix geek and <i class=\"fa fa-music\"></i> Musician"
+  , "domain": "https://ionicabizau.net"
+  , "plugins": [
+      "emoji",
+    , "lightbox"
+    , "social"
+    , ["rss", { "description": "WEB Developer, *nix Geek and Musician" }]
+    , ["redirect", {
+        "/blog/26-uploading-videos-to-youtube-using-nodejs": "/blog/14-uploading-videos-to-youtube-using-nodejs"
+      , "/blog/34-the-joy-of-being-a-mentor": "/blog/35-the-joy-of-being-a-mentor"
+      }]
+    , ["custom-assets", {
+          "server": [
+              "server/resume.js"
+          ]
+      }]
+    //, ["contact-form", {
+    //     "api_key": process.env.SG_API_KEY,
+    //     "contact": {
+    //         "email": "contact@ionicabizau.net",
+    //         "name": "Ionică Bizău"
+    //     }
+    //  }]
     ]
-  , config: {
-        bloggifyRouter: {
-            paths: {
-                blog: "/"
-              , articlePath: "/blog"
+  , "adapter": ["bloggify-markdown-adapter", {
+        "routes": {
+          "articles": "/blog",
+          "blog_url": "/"
+        },
+        "theme": ["theme-light", {
+            "options": {
+                "social": {
+                    "twitter": "IonicaBizau",
+                    "youtube": "IonicaBizau",
+                    "github": "IonicaBizau",
+                    "bitbucket": "IonicaBizau"
+                },
+                "ga": {
+                    "id": "UA-34774185-1",
+                    "url": "ionicabizau.net"
+                }
             }
-        },
-        bloggifyRedirect: {
-           "/blog/26-uploading-videos-to-youtube-using-nodejs": "/blog/14-uploading-videos-to-youtube-using-nodejs",
-           "/blog/34-the-joy-of-being-a-mentor": "/blog/35-the-joy-of-being-a-mentor"
-        },
-        bloggifyCustomAssets: {
-           server: [
-               "server/resume.js"
-           ]
-        },
-        bloggifyContactForm: {
-            api_key: process.env.SG_API_KEY,
-            contact: {
-                email: "contact@ionicabizau.net",
-                name: "Ionică Bizău"
-            }
-        },
-        bloggifyRss: {
-            description: "WEB Developer, *nix Geek and Musician"
-        }
-    }
-}, {
-    theme: {
-        social: {
-            twitter: "IonicaBizau",
-            youtube: "IonicaBizau",
-            github: "IonicaBizau",
-            bitbucket: "IonicaBizau"
-        },
-        ga: {
-            id: "UA-34774185-1",
-            url: "ionicabizau.net"
-        }
-    }
-});
+        }]
+    }]
+}
